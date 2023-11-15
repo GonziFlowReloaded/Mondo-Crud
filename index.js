c3aa0fba9534068d2860de5da568f0fdd14b3c96
Facundo Almaraz <facundoalmaraz8@gmail.com>
2023-11-14 22:11:37 -0300

    Cambiando CORS

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
-import mesaRoutes from "./routes/mesaRoutes.js"
-import usuarioRoutes from "./routes/usuarioRoutes.js"
+import mesaRoutes from "./routes/mesaRoutes.js";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
 const app = express();
-app.use(express.json())
-const PORT = process.env.PORT || 4000
+app.use(cors());
 
-dotenv.config()
+app.use(express.json());
+const PORT = process.env.PORT || 4000;
 
-conectarDB()
-app.get('/', (req, res) => {
-  res.send('Hola, mundo!');
+dotenv.config();
+
+conectarDB();
+app.get("/", (req, res) => {
+  res.send("Hola, mundo!");
 });
 
-
-app.use(cors())
 //Routing
 
-app.use("/api/usuarios", usuarioRoutes)
-app.use("/api/mesas", mesaRoutes)
+app.use("/api/usuarios", usuarioRoutes);
+app.use("/api/mesas", mesaRoutes);
 
 app.listen(PORT, () => {
   console.log(`El servidor está escuchando en http://localhost:${PORT}`);
